78dbd2118dde4be7a5c475477f7e259ef8897bb0
A. F. M Shahriar Sharif <128410048+ShahriarSharif@users.noreply.github.com>
2023-03-21 02:41:20 +0600

    Add files via upload

---
+++ UIU-ICS-PROGRAMMING-ASSIGNMENT/Basic_Introductory_Problems/8th.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int first_num, middle_num, last_num;
+
+    scanf("%d %d %d", &first_num, &middle_num, &last_num);
+
+    printf("First Value = %d,  Last Value = %d", first_num, last_num);
+
+    return 0;
+}
---
+++ UIU-ICS-PROGRAMMING-ASSIGNMENT/Basic_Introductory_Problems/9th.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    double a;
+    bool b;
+
+    a = 1.618039;
+    b = true;
+
+
+    printf("The double value: %lf \n", a);
+    printf("The boolean value: %d", b);
+
+    return 0;
+}
